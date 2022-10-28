a4acd9d1c5a21f7ae09a70f39ec36c36e3e756cb
mxkov <hawo0451@gmail.com>
2022-10-28 15:43:39 +0200

    Adjust path to trained model dir

--- runs/run_localtest.sh
+++ runs/run_localtest.sh
@@ -2,7 +2,7 @@
 
 singularity exec --nv singularity.sif python3 run_gcae.py train \
 --datadir data/HOpheno_249ind_1000snp/ --data HOpheno_249ind_1000snp \
---trainedmodeldir ae_out_localtest/ \
+--trainedmodeldir ae_out/LOCALTEST/ \
 --model_id M1 --train_opts_id ex3 --data_opts_id b_0_4 \
 --epochs 10 --save_interval 2 \
 --pheno_model_id ph1 \
