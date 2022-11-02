c4b212b70e4000862302be726e81b4dd6b72e397
mxkov <hawo0451@gmail.com>
2022-11-02 16:42:26 +0100

    Add projecting to the local testing runscript

--- runs/run_localtest.sh
+++ runs/run_localtest.sh
@@ -5,6 +5,12 @@ singularity exec --nv singularity.sif python3 run_gcae.py train \
 --trainedmodeldir ae_out/LOCALTEST/ \
 --model_id M1 --train_opts_id ex3 --data_opts_id b_0_4 \
 --epochs 10 --save_interval 2 \
---pheno_model_id ph1 \
-2>&1 | tee runs/logs/localtest.log
+2>&1 | tee runs/logs/localtest_train.log
+
+singularity exec --nv singularity.sif python3 run_gcae.py project \
+--datadir data/HOpheno_249ind_1000snp/ --data HOpheno_249ind_1000snp \
+--trainedmodeldir ae_out/LOCALTEST/ \
+--model_id M1 --train_opts_id ex3 --data_opts_id b_0_4 \
+--superpops data/HOpheno_249ind_1000snp/HO_superpopulations \
+2>&1 | tee runs/logs/localtest_project.log
 
