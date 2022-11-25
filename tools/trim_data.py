5bfc86a333c24f08732551e12a2bb9aa254a9fba
mxkov <hawo0451@gmail.com>
2022-11-25 16:58:47 +0100

    Enable random sampling of SNPs
    Now tools/trim_data.py selects a random sample of m SNPs instead of the
    first m SNPs.

--- tools/trim_data.py
+++ tools/trim_data.py
@@ -7,24 +7,29 @@ Options:
   -h --help            show this screen
   --dataprefix=<name>  path and prefix of the data in the EIGENSTRAT format. if not absolute: assumed relative to GenoCAE/ directory
   --outprefix=<name>   path and prefix for the output data. if not absolute: assumed relative to GenoCAE/ directory
-  -m <num>             number of SNPs to cut to. DEFAULT: all SNPs
+  -m <num>             number of SNPs to cut to. randomly sampled without replacement, order preserved. DEFAULT: all SNPs
   -n <num>             number of samples per population to cut to. DEFAULT: all samples
   -k <num>             if specified, cut the TOTAL number of samples further down to k. DEFAULT: not specified
 
 '''
 
 from docopt import docopt, DocoptExit
-import os, sys
+import os
 import pandas as pd
 from pathlib import Path
+from random import seed, sample
 from shutil import copy2
 from tabulate import tabulate
+
+
+def reportOutfile(filepath, margin = ''):
+	print('{}File written: {}'.format(margin, filepath))
 
 
 ## Parsing args
 
 try:
-	arguments = docopt(__doc__, version='DataTrim 0.1.0')
+	arguments = docopt(__doc__, version='DataTrim 0.2.0')
 except DocoptExit:
 	print('Invalid command. Run `python3 trim_data.py --help` for more information.')
 	exit(1)
@@ -35,7 +40,7 @@ for x in list(arguments.keys()):
 
 argtypes = {'dataprefix': 'path', 'outprefix': 'path',
             'm': 'num', 'n': 'num', 'k': 'num'}
-margin = '\n'
+margin1 = '\n'
 for arg in argtypes.keys():
 	if arguments[arg]:
 		try:
@@ -44,8 +49,8 @@ for arg in argtypes.keys():
 			elif argtypes[arg] == 'path':
 				exec('{} = "{}"'.format(arg, arguments[arg]))
 		except:
-			print('{}Warning: Invalid argument {}'.format(margin, arg))
-			margin = ''
+			print('{}Warning: Invalid argument {}'.format(margin1, arg))
+			margin1 = ''
 			exec('{} = None'.format(arg))
 	else:
 		exec('{} = None'.format(arg))
@@ -71,29 +76,30 @@ if m is None and n is None and k is None:
 	print('\nWarning: m, n, k are not specified, copying files without changes')
 	for ext in ('.snp', '.ind', '.eigenstratgeno'):
 		copy2(dataprefix + ext, outprefix + ext)
-		print('File written: {}'.format(outprefix + ext))
+		reportOutfile(outprefix + ext)
 	exit()
 
 # snips:
-# TODO: take a random subset of m snips rather than the first m snips
 if m is None:
 	print('\nWarning: m is not specified, using all SNPs')
 	copy2(dataprefix + '.snp', outprefix + '.snp')
-	margin = ''
+	margin1 = ''
 else:
 	snp = pd.read_table(dataprefix + '.snp', header = None, delimiter=r'\s+')
-	snp = snp[:m]
+	seed(42)
+	snips_sampled = sorted(sample(range(snp.shape[0]), m))
+	snp = snp.iloc[snips_sampled]
 	f = open(outprefix + '.snp', 'w')
 	f.write(tabulate(snp.values.tolist(), tablefmt = 'plain'))
 	f.close()
-	margin = '\n'
-print('{}File written: {}'.format(margin, outprefix + '.snp'))
+	margin1 = '\n'
+reportOutfile(outprefix + '.snp', margin = margin1)
 
 # individuals:
 if n is None and k is None:
 	print('\nWarning: neither n nor k are specified, using all individuals')
 	copy2(dataprefix + '.ind', outprefix + '.ind')
-	margin = ''
+	margin1 = ''
 else:
 	ind = pd.read_table(dataprefix + '.ind', header = None, delimiter=r'\s+')
 	if n is None:
@@ -113,20 +119,20 @@ else:
 	ind1 = pd.DataFrame(ind1_list)
 	ind1.to_csv(outprefix + '.ind',
 	            header = None, index = False, sep = '\t')
-	margin = '\n'
-print('{}File written: {}'.format(margin, outprefix + '.ind'))
+	margin1 = '\n'
+reportOutfile(outprefix + '.ind', margin = margin1)
 
 # genomic:
 f = open(dataprefix + '.eigenstratgeno', 'r')
 esg = f.read().split('\n')[:-1]
 f.close()
 if m is not None:
-	esg = esg[:m]
+	esg = [esg[i] for i in snips_sampled]
 if n is not None or k is not None:
 	for i in range(len(esg)):
 		esg[i] = ''.join([esg[i][j] for j in ind1_indx])
 f = open(outprefix + '.eigenstratgeno', 'w')
 f.write('\n'.join(esg))
 f.close()
-print('\nFile written: {}'.format(outprefix + '.eigenstratgeno'))
+reportOutfile(outprefix + '.eigenstratgeno', margin = '\n')
 
